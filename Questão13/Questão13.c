9d046f58a77c86624bb38c47d9adc60661a57c52
Geovana Beatriz <107216962+Calopsitaaaa@users.noreply.github.com>
2022-10-20 23:04:38 -0300

    Questão13

---
+++ Questão13/Questão13.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{   float m,k;
+    printf("digite a velocidade em k\n");
+    scanf("%g",&k);
+    m=k/1.61;
+    printf("equivale %g m",m,k);
+    return 0;
+}
