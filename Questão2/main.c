78c493f66124720302d36260630c3e377ef5c22b
Geovana Beatriz <107216962+Calopsitaaaa@users.noreply.github.com>
2022-10-20 22:24:53 -0300

    Quetão2

---
+++ Questão2/main.c
@@ -0,0 +1,16 @@
+//
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+float x;
+
+printf ( "digite um numero real: " );
+scanf ( "%g",&x ) ;
+
+printf ( "numero digitado = %g\n",x );
+
+return 0;
+
+}
