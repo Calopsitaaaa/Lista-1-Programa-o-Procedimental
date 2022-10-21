e8744dd677d75a1a82b48e611ef4f33d2f179e3c
Geovana Beatriz <107216962+Calopsitaaaa@users.noreply.github.com>
2022-10-20 22:18:22 -0300

    Questão1

---
+++ Questão1/Quetão1int.c
@@ -0,0 +1,14 @@
+// 01. Faça um programa que leia um número inteiro e o imprima.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int x;
+    printf("digite um numero inteiro:");
+    scanf("%d",&x);
+
+    printf("numero digitado:%d",x);
+
+    return 0;
+}
